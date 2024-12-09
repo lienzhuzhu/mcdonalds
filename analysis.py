57d27ad780a3e8d3bac10365ccf4a64a7cb189de
Lien Zhu <lienzhucs@gmail.com>
2024-12-09 03:47:41 -0600

    adds heart attack data

--- analysis.py
+++ analysis.py
@@ -2,15 +2,22 @@ from os import path
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 DATA_FOLDER = "~/armory/mcdonalds/data/"
 DATA_FILE = "McDonaldsMenuNutritionV3.csv"
 
+def general_stats(data):
+    print(data.head())
+    print(data.shape)
+    print(data.info())
+    print(data.describe())
 
 def main():
     raw_data = pd.read_csv(path.join(DATA_FOLDER, DATA_FILE))
-    print(raw_data.columns)
+    corr = raw_data.corr(numeric_only=True)
+    print(corr)
 
 
 if __name__ == "__main__":
