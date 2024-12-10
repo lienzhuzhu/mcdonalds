93188ee0091cef5fb54797078416a4fb98158391
Lien Zhu <lienzhucs@gmail.com>
2024-12-10 01:25:42 -0600

    Cleaned data

--- cleaner.py
+++ cleaner.py
@@ -1,8 +1,10 @@
 import pandas as pd
+import re
+import csv
 
 # Load the dataset
 file_path = "./data/mcds.csv"  # Adjust the file path as needed
-mcds = pd.read_csv(file_path)
+mcds = pd.read_csv(file_path, encoding="utf-8")
 
 # Rename columns to simpler names
 mcds.columns = [
@@ -21,8 +23,19 @@ mcds.columns = [
     "weight_watchers_points"
 ]
 
-# Save the renamed dataset to a new file for use in R or Python
+
+# Clean up item names by removing trademark symbols
+def clean_item_name(item_name):
+    # Remove ™ and ® symbols
+    item_name = re.sub(r'\n', ' ', item_name)
+    return re.sub(r'[™®]', '', item_name)
+
+# Apply the cleaning function to the item column
+mcds['item'] = mcds['item'].apply(clean_item_name)
+
+# Save the cleaned and renamed dataset to a new file
 output_file = "./data/mcds_clean.csv"
 mcds.to_csv(output_file, index=False)
+
 
 print(f"Renamed columns and saved the file to {output_file}")
--- eda.R
+++ eda.R
@@ -1,14 +1,8 @@
 source("./setup.R")
 
 mcds <- read.csv("./data/mcds_clean.csv", header=TRUE)
-cat(sprintf("Protein and Calories:\t%f\n", cor(mcds$protein, mcds$calories)))
-cat(sprintf("Calories and Total Fat:\t%f\n", cor(mcds$calories, mcds$total_fat_g)))
-
 
 mcds$other_fat <- mcds$total_fat_g - (mcds$saturated_fat_g + mcds$trans_fat_g)
 mcds$weight_watchers_points <- NULL
 
-
-print(head(mcds[, c(1:2, (ncol(mcds) - 1):ncol(mcds))]))
-
-View(mcds)
+print(sapply(mcds, class))
